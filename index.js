5d268d1b3c6005ee308f75feaa28d2b55bdcc906
Deborah <liz.joy43@gmail.com>
2024-08-30 08:59:32 -0600

    fixed my CSS, and added some Javascript

--- index.js
+++ index.js
@@ -1,1 +1,3 @@
-
+let losAngelesElement = document.querySelector("#los-angeles");
+let losAngelesDateElement = losAngelesElement.querySelector(".date");
+losAngelesDateElement.innerHTML = "August 30th, 2024";
