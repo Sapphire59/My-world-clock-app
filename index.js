5e5d75e2ac741e08fb51b541678d6c61a954b9d4
Deborah <liz.joy43@gmail.com>
2024-08-30 09:24:52 -0600

    fixed CSS, added Javascript

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 let losAngelesElement = document.querySelector("#los-angeles");
 let losAngelesDateElement = losAngelesElement.querySelector(".date");
 losAngelesDateElement.innerHTML = "August 30th, 2024";
+let losAngelesTimeElement
