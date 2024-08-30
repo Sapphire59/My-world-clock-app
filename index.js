f69a55cc21f12b1c8e29de66f41f69377d499d65
Deborah <liz.joy43@gmail.com>
2024-08-30 10:30:53 -0600

    added current-time in Javascript

--- index.js
+++ index.js
@@ -1,4 +1,21 @@
-let losAngelesElement = document.querySelector("#los-angeles");
-let losAngelesDateElement = losAngelesElement.querySelector(".date");
-losAngelesDateElement.innerHTML = "August 30th, 2024";
-let losAngelesTimeElement
+function updateTime() {
+  let losAngelesElement = document.querySelector("#los-angeles");
+  let losAngelesDateElement = losAngelesElement.querySelector(".date");
+  losAngelesDateElement.innerHTML = "August 30th, 2024";
+  let losAngelesTimeElement = losAngelesElement.querySelector(".time");
+  losAngelesTime = moment().tz("America/Los_Angeles");
+  losAngelesDateElement.innerHTML = losAngelesTime.format("MMMM Do, YYYY");
+  losAngelesTimeElement.innerHTML = losAngelesTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
+
+  let parisElement = document.querySelector("#paris");
+  let parisDateElement = parisElement.querySelector(".date");
+  parisDateElement.innerHTML = "August 30th, 2024";
+  let parisTimeElement = parisElement.querySelector(".time");
+  parisTime = moment().tz("Europe/Paris");
+  parisDateElement.innerHTML = parisTime.format("MMMM Do, YYYY");
+  parisTimeElement.innerHTML = parisTime.format("h:mm:ss [<small>]A[</small>]");
+}
+updateTime();
+setInterval(updateTime, 1000);
